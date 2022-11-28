7733c275d5a2342c64227a126ea9eacea4bb0ec2
teddyoweh <teddyoweh@gmail.com>
2022-11-27 22:23:08 -0600

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from flask_cors import CORS
 app = create_app('config.development')
 
 if __name__ == '__main__':
-    app.run(host='0:0:0:0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
